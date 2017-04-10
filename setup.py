53deb0400b30ac2622fe3e8cb871375d337f854c
Kevin Walchko <kevin@Logan.local>
2017-04-09 22:34:06 -0600

    init

---
+++ setup.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+from setuptools import setup
+from build_utils import __version__ as VERSION
+from build_utils import BuildCommand
+from build_utils import PublishCommand
+from build_utils import BinaryDistribution
+
+PACKAGE_NAME = 'build_utils'
+BuildCommand.pkg = PACKAGE_NAME
+BuildCommand.test = False
+PublishCommand.pkg = PACKAGE_NAME
+PublishCommand.version = VERSION
+README = open('README.rst').read()
+
+setup(
+	name=PACKAGE_NAME,
+	version=VERSION,
+	author="Kevin Walchko",
+	keywords=['framework', 'python2', 'python3'],
+	author_email="kevin.walchko@outlook.com",
+	description="Some tools to help build python2 and python3 libraries",
+	license="MIT",
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.6',
+		'Operating System :: Unix',
+		'Operating System :: POSIX :: Linux',
+		'Operating System :: MacOS :: MacOS X',
+		'Operating System :: POSIX',
+		# 'Topic :: Scientific/Engineering',
+		# 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+		# 'Topic :: Scientific/Engineering :: Image Recognition',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
+	install_requires=[],
+	url="https://github.com/walchko/{}".format(PACKAGE_NAME),
+	long_description=README,
+	packages=[PACKAGE_NAME],
+	cmdclass={
+		'publish': PublishCommand,
+		'make': BuildCommand
+	}
+)
