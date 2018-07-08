23d913c224292ed2b4b378b048f49cd4257234dd
walchko <walchko@users.noreply.github.com>
2018-07-08 10:54:35 -0600

    added colorization and setting git tag

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from build_utils import __version__ as VERSION
 from build_utils import BuildCommand
 from build_utils import PublishCommand
 from build_utils import BinaryDistribution
+from build_utils import SetGitTag
 
 
 PACKAGE_NAME = 'build_utils'
@@ -11,33 +12,36 @@ BuildCommand.pkg = PACKAGE_NAME
 BuildCommand.test = False  # there are no tests
 PublishCommand.pkg = PACKAGE_NAME
 PublishCommand.version = VERSION
-README = open('README.rst').read()
+SetGitTag.version = VERSION
+README = open('readme.md').read()
 
 setup(
-	name=PACKAGE_NAME,
-	version=VERSION,
-	author="Kevin Walchko",
-	keywords=['framework', 'python2', 'python3'],
-	author_email="walchko@users.noreply.github.com",
-	description="Some tools to help build python2 and python3 libraries",
-	license="MIT",
-	classifiers=[
-		'Development Status :: 4 - Beta',
-		'License :: OSI Approved :: MIT License',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.6',
-		'Operating System :: Unix',
-		'Operating System :: POSIX :: Linux',
-		'Operating System :: MacOS :: MacOS X',
-		'Operating System :: POSIX',
-		'Topic :: Software Development :: Libraries :: Python Modules'
-	],
-	install_requires=['nose', 'twine'],
-	url="https://github.com/walchko/{}".format(PACKAGE_NAME),
-	long_description=README,
-	packages=[PACKAGE_NAME],
-	cmdclass={
-		'publish': PublishCommand,
-		'make': BuildCommand
-	}
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author="Kevin Walchko",
+    keywords=['framework', 'python2', 'python3'],
+    author_email="walchko@users.noreply.github.com",
+    description="Some tools to help build python2/3 source and wheel libraries",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Unix',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    install_requires=['nose', 'twine', 'colorama'],
+    url="https://github.com/walchko/{}".format(PACKAGE_NAME),
+    long_description=README,
+    long_description_content_type='text/markdown',
+    packages=[PACKAGE_NAME],
+    cmdclass={
+        'publish': PublishCommand,
+        'make': BuildCommand,
+        'git': SetGitTag
+    }
 )
